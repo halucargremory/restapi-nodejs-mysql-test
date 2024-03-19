ffdc615ac8f04c07d6f949479063f305ecde27c6
halucargremory <82543684+halucargremory@users.noreply.github.com>
2024-03-19 17:27:36 -0600

    actualizacion conexion servidor

--- src/config.js
+++ src/config.js
@@ -7,4 +7,4 @@ export const DB_HOST = process.env.DB_HOST || localhost
 export const DB_PORT = process.env.DB_PORT || 3306
 export const DB_USE = process.env.DB_USE || root
 export const DB_PASSWORD = process.env.DB_PASSWORD || MOTOCICLETA10
-export const DB_DATABASE = process.env.DB_DATABASE || procomin_epos
+export const DB_DATABASE = process.env.DB_DATABASE || personal_procomin
